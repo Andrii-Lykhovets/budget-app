501ea2b2967c29edd3b484a698b8c6e90dd0420a
Andrew <144724707+Andrii-Lykhovets@users.noreply.github.com>
2024-06-10 19:22:45 +0200

    add gitignore and checkpoint of all code up to date

--- budget_app/example/views.py
+++ budget_app/example/views.py
@@ -14,3 +14,23 @@ def simple_lesson_view(request):
 
 def homework_view(request):
     return HttpResponse('This is my homework!')
+
+
+def render_template_example(request):
+    return render(request, 'example/template_example.html')
+
+
+def render_templated_with_variables(request):
+    rendered_variables = {
+        'example_var': 'This is how the story went: I met someone by accident.',
+        'a_list': [1, 2, 3],
+        'a_dict': {
+            'random': 42,
+            'key1': 'that opens the door 1',
+        }
+    }
+    return render(
+        request,
+        'example/template_with_variables.html',
+        context=rendered_variables,
+    )
